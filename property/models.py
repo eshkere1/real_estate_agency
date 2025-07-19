3ad820fa703dd7d55c762084459785f1d1d3f932
Pavel <piv17032009@gmail.com>
2025-07-19 13:23:42 +0300

    setup Flat changelist page

--- property/models.py
+++ property/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    new_building = models.BooleanField('Новое здание', null=True, blank=False)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
