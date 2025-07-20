c6b2d9b80bc61c83787d36649bb1ad5d1ce2c258
Pavel <piv17032009@gmail.com>
2025-07-20 14:28:59 +0300

    fill with data field Flat.construction_year

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ from .models import Flat
 class AuthorAdmin(admin.ModelAdmin):
     search_fields = ("town", "address", "owner")
     readonly_fields = ["created_at"]
-    list_display = ["address", "price", "new_building", "construction_year", "town"]
+    list_display = ("address", "price", "new_building", "construction_year", "town")
     list_editable = ["new_building"]
 
 
--- property/models.py
+++ property/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
-    new_building = models.BooleanField('Новое здание', null=True, blank=False)
+    new_building = models.BooleanField('Новое здание', null=True, blank=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
